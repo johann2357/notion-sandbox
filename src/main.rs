732ca93c0b44d2657249e6ceac7e2f5c05afb2c5
Johann Nunez <johann2357@gmail.com>
2023-05-01 22:18:12 -0500

    feat: add database query for pages
    implement retrieving notion pages that match a given query

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use serde_json::json;
 use std::env;
 
 use dotenv::dotenv;
@@ -7,12 +8,41 @@ mod notion;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Load the environment variables from the .env file
     dotenv().ok();
-    let _notion_token =
-        env::var("NOTION_TOKEN").expect("Environment variable NOTION_TOKEN not set");
+    let notion_token = env::var("NOTION_TOKEN").expect("Environment variable NOTION_TOKEN not set");
+    let database_id = "c6c99573-b3ad-4b1d-ac6e-d8c7f7f43007".to_string();
+    let body = json!({
+        "filter": {
+            "or": [
+                {
+                    "property": "Name",
+                    "rich_text": {
+                        "contains": "Notas 2023-03-24"
+                    }
+                },
+                {
+                    "property": "Fecha",
+                    "date": {
+                        "equals": "2023-03-24"
+                    }
+                }
+            ]
+        },
+        "sorts": [
+            {
+                "property": "Created",
+                "direction": "ascending"
+            }
+        ]
+    });
 
-    println!("Hello, world!");
+    let response = notion::database::find_pages(&database_id, body, &notion_token)
+        .await
+        .expect("Failed to get pages");
+
+    for page in response.results {
+        println!("{:#?}", page);
+    }
 
     Ok(())
 }
--- src/notion/database.rs
+++ src/notion/database.rs
@@ -1,1 +1,77 @@
+use std::collections::HashMap;
 
+use reqwest;
+use serde::{Deserialize, Serialize};
+
+use super::errors::NotionError;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub object: String,
+    pub id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Icon {
+    #[serde(rename = "type")]
+    pub icon_type: String,
+    pub emoji: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Parent {
+    #[serde(rename = "type")]
+    pub parent_type: String,
+    pub database_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Page {
+    pub object: String,
+    pub id: String,
+    pub created_time: String,
+    pub last_edited_time: String,
+    pub created_by: User,
+    pub last_edited_by: User,
+    pub cover: Option<String>,
+    pub icon: Icon,
+    pub parent: Parent,
+    pub archived: bool,
+    pub properties: HashMap<String, serde_json::Value>,
+    pub url: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QueryResponse {
+    pub object: String,
+    pub results: Vec<Page>,
+    pub next_cursor: Option<String>,
+    pub has_more: bool,
+    #[serde(rename = "type")]
+    pub response_type: String,
+    pub page: HashMap<String, serde_json::Value>,
+}
+
+pub async fn find_pages(
+    database_id: &String,
+    body: serde_json::Value,
+    token: &String,
+) -> Result<QueryResponse, NotionError> {
+    let url = format!("https://api.notion.com/v1/databases/{}/query", database_id);
+    let client = reqwest::Client::new();
+    let response = client
+        .post(&url)
+        .header("Content-Type", "application/json")
+        .header("Notion-Version", "2022-06-28")
+        .header("Authorization", format!("Bearer {}", token))
+        .body(body.to_string())
+        .send()
+        .await
+        .map_err(NotionError::Reqwest)?;
+
+    let response_text = response.text().await.map_err(NotionError::Reqwest)?;
+
+    let file: QueryResponse = serde_json::from_str(&response_text).map_err(NotionError::Serde)?;
+
+    return Ok(file);
+}
